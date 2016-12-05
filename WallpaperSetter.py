0e82c2a43a1f45d87658d3923ec6de8e5372475c
Sukrit Arora <Sukrit@airbears2-10-142-15-122.airbears2.1918.berkeley.edu>
2016-12-04 20:12:57 -0800

    Initial commit

---
+++ WallpaperSetter.py
@@ -0,0 +1,22 @@
+import praw
+from urllib.request import urlopen
+from io import BytesIO
+from PIL import Image
+from datetime import datetime
+
+user_agent = ("Wallpaper Script by /u/sukibae 0.1")
+r = praw.Reddit(client_id = "nMeXsi0VEEaW_A", user_agent = user_agent, client_secret = "E6PLfbBEs_CyKqlFYgxe046fop8")
+
+for post in r.subreddit("earthporn").hot(limit = 5):
+    response = urlopen(post.url)
+    data = response.read()
+    print(post.title)
+    try:
+        image = Image.open(BytesIO(data))
+        image.verify()
+        fname= "Images/" + str(datetime.now()) + ".jpg"
+        Image.open(BytesIO(data)).save(fname)
+    except:
+        # Encountered issue downloading image. Image is probably somewhere in the page
+        # or might not be a filetype supported by PIL
+        pass
